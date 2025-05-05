749296c7ccfad942bd6be1e8fff8a7b3b132c883
rajchovatiya <rajchovatiya783@gmail.com>
2025-05-05 10:10:51 +0530

    add code AD code

--- adsctip.js
+++ adsctip.js
@@ -11,35 +11,13 @@ function loadAd() {
 
 
 
-// let clike = document.querySelector("window");
-// let chak = 0;
-// window.addEventListener("click", () => {
-//     chak++
-
-//     let localstorage = localStorage.getItem("adclicks");
-//     if (localstorage == 3) {
-//         let adContainer = document.getElementById("AD_class1");
-//         adContainer.style.display = "block";
-//         blur.style.filter = "blur(5px)";
-//     } else if (localstorage == 4) {
-//         chak = 1;
-
-//     }
-//     localStorage.setItem("adclicks", chak);
-
-// });
 const pageKey = 'index_visits';
 let visits = localStorage.getItem(pageKey) || 0;
 visits = parseInt(visits) + 1;
 localStorage.setItem(pageKey, visits);
 
-if (visits == 3) {
+if (visits % 3 == 0) {
     let adContainer = document.getElementById("AD_class1");
     adContainer.style.display = "block";
     blur.style.filter = "blur(5px)";
-
-} else if (visits == 4) {
-    visits = 1
-    localStorage.setItem(pageKey, visits);
-
-}
+} 
